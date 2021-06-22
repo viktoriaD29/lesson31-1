f505c890954c5cd2207d1484ef44f4a6d5be8d3f
Viktoria <doduchenko29@gmail.com>
2021-06-22 17:12:03 +0300

    add

---
+++ index.js
@@ -0,0 +1,26 @@
+export const requestUserDate = userId => {
+  const request = new Promise((resolve, reject) => {
+    if (userId === 'broken') {
+      setTimeout(() => {
+        reject(new Error('User not found'))
+      }, 500)
+    } else {
+      setTimeout(() => {
+        resolve({
+          name: 'John',
+          age: 17,
+          userId: `${userId}`,
+          email: `${userId}@example.com`,
+        });
+      }, 1000);
+    }
+    
+  })
+
+  return request
+}
+
+/*requestUserDate('broken')
+  .then(date => console.log(date))
+  .catch(error => console.log(error))
+  .finally(() => console.log('finally'))*/
